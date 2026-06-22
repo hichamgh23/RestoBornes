5e0bee431c25d87bd0336741b406642316e10183
Hicham Ghassoul <hicham.ghassoul2018@gmail.com>
2026-06-22 16:33:19 +0200

    feat: setup API structure and initial product retrieval

---
+++ RestoBorne.Api/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestoBorne.Application.Interfaces; // N'oublie pas cet import !
+
+namespace RestoBorne.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        // Le constructeur reçoit le repository injecté par .NET
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // La méthode qui répond aux requêtes GET
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _repository.GetAllAsync();
+            return Ok(products); // Retourne la liste en JSON
+        }
+    }
+}
+
--- RestoBorne.Api/Program.cs
+++ RestoBorne.Api/Program.cs
@@ -1,12 +1,13 @@
+using RestoBorne.Application.Interfaces; 
+using RestoBorne.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +15,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/api/products", async (IProductRepository repository) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var products = await repository.GetAllAsync();
+    return Results.Ok(products);
 })
-.WithName("GetWeatherForecast");
+.WithName("GetAllProducts");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
