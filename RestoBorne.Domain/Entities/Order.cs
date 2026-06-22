913c412be99ddf56161a75a27fa331bbb2ad6de6
Hicham Ghassoul <hicham.ghassoul2018@gmail.com>
2026-06-22 17:18:28 +0200

    feat: create Order and OrderItem entities

---
+++ RestoBorne.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestoBorne.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        // Utilise un Enum pour éviter les erreurs de saisie
+        public string Status { get; set; } = "Pending";
+
+        // La relation : Une commande a plusieurs lignes de commande
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public decimal TotalAmount { get; set; }
+    }
+}
